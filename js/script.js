f15ccb764e6f6d90f152a176ff21b71c55866d33
Lolloc99 <lolloc99@yahoo.it>
2022-04-08 16:13:15 +0200

    Apertura ticket

--- js/script.js
+++ js/script.js
@@ -8,3 +8,17 @@ Il recap dei dati e l'output del prezzo finale va stampato in pagina (formattato
 Consigli:
 Iniziate il lavoro con html base, implementate la logica e le iterazioni con utente. Soltanto quando siete sicuri che funziona tutto passate all'aspetto grafico.
 */
+
+// Compilazione km da percorrere e età cliente
+const nomeCognome = document.getElementById('nome-cognome').value;
+const trattakm= document.getElementById('percorso').value;
+
+const prezzo = trattakm * 0.21
+console.log(prezzo);
+
+// Calcolo prezzo biglietto
+// prezzo standard = NumerKM x 0.21€
+// Se selezionato "minorenne" allora Prezzo - 20%
+// Se selezionato "over65" allora Prezzo - 40%
+
+// stampare il tutto su schermo negli appositi spazi
